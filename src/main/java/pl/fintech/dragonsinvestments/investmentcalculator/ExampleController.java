a7b2eb21846dc5f4c7619f1b6825dacc3813f6f9
Maciej Parapura <mparapura98@gmail.com>
2020-10-05 00:33:00 +0200

    ERSA-21 Swagger and cors config
    Done:
    - add swagger config
    - add cors config

--- src/main/java/pl/fintech/dragonsinvestments/investmentcalculator/ExampleController.java
+++ src/main/java/pl/fintech/dragonsinvestments/investmentcalculator/ExampleController.java
@@ -3,7 +3,9 @@ package pl.fintech.dragonsinvestments.investmentcalculator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Example controller")
 @RestController
 @RequestMapping("/")
 public class ExampleController {
