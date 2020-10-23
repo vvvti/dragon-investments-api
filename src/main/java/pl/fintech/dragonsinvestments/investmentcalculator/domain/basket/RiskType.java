d7b8327a61ff1f023bf6648870624ebc0c65a007
Krystian Soloducha <krystsolo@gmail.com>
2020-10-23 13:57:11 +0200

    ERSA-96 serialize enums to uppercase and deserialize with case insensitive

--- src/main/java/pl/fintech/dragonsinvestments/investmentcalculator/domain/basket/RiskType.java
+++ src/main/java/pl/fintech/dragonsinvestments/investmentcalculator/domain/basket/RiskType.java
@@ -1,33 +1,40 @@
 package pl.fintech.dragonsinvestments.investmentcalculator.domain.basket;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import java.math.BigDecimal;
 
-public enum RiskType {
-  VeryConservative(0.2, 0.5, 0.3),
-  Conservative(0.45, 0.4, 0.15),
-  Moderate(0.65, 0.30, 0.05),
-  Aggressive(0.80, 0.15, 0.05),
-  VeryAggressive(0.9, 0.05, 0.05);
+enum RiskType {
+    VeryConservative(0.2, 0.5, 0.3),
+    Conservative(0.45, 0.4, 0.15),
+    Moderate(0.65, 0.30, 0.05),
+    Aggressive(0.80, 0.15, 0.05),
+    VeryAggressive(0.9, 0.05, 0.05);
 
-  private BigDecimal cashPart;
-  private BigDecimal bondsPart;
-  private BigDecimal stocksPart;
+    private final BigDecimal cashPart;
+    private final BigDecimal bondsPart;
+    private final BigDecimal stocksPart;
 
-  RiskType(Double cashPart, Double bondsPart, Double stocksPart) {
-    this.cashPart = BigDecimal.valueOf(cashPart);
-    this.bondsPart = BigDecimal.valueOf(bondsPart);
-    this.stocksPart = BigDecimal.valueOf(stocksPart);
-  }
+    RiskType(Double cashPart, Double bondsPart, Double stocksPart) {
+        this.cashPart = BigDecimal.valueOf(cashPart);
+        this.bondsPart = BigDecimal.valueOf(bondsPart);
+        this.stocksPart = BigDecimal.valueOf(stocksPart);
+    }
 
-  BigDecimal cashPart(){
-    return this.cashPart;
-  }
+    BigDecimal cashPart() {
+        return this.cashPart;
+    }
 
-  BigDecimal bondsPart(){
-    return this.bondsPart;
-  }
+    BigDecimal bondsPart() {
+        return this.bondsPart;
+    }
 
-  BigDecimal stocksPart(){
-    return this.stocksPart;
-  }
+    BigDecimal stocksPart() {
+        return this.stocksPart;
+    }
+
+    @JsonValue
+    public String getType(){
+        return this.name().toUpperCase();
+    }
 }
