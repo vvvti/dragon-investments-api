d5ee52f456bb66da634cf70f95b897ea44003569
Krystian Soloducha <krystsolo@gmail.com>
2020-10-26 01:27:56 +0100

    ERSA-96 change basketDto property name

--- src/main/java/pl/fintech/dragonsinvestments/investmentcalculator/domain/basket/BasketDto.java
+++ src/main/java/pl/fintech/dragonsinvestments/investmentcalculator/domain/basket/BasketDto.java
@@ -1,5 +1,6 @@
 package pl.fintech.dragonsinvestments.investmentcalculator.domain.basket;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import javax.validation.constraints.DecimalMin;
@@ -11,6 +12,7 @@ class BasketDto {
 
     @DecimalMin(value = "0", inclusive = false, message = "Basket value must be greater than zero")
     @NotNull
+    @JsonProperty("basketValue")
     BigDecimal value;
 
     @NotNull
